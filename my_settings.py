dcd3bd166b6be4ebffa60b228131da7a57acf1bb
HanJoon Kim <hj@HanJoonui-MacBookPro.local>
2021-06-18 15:11:20 +0900

    Add: Django Project Setting

---
+++ my_settings.py
@@ -0,0 +1,12 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'coach',
+        'USER': 'root',
+        'PASSWORD': '9173',
+        'HOST': '127.0.0.1', #localhost
+        'PORT': '3306',
+    }
+}
+
+SECRET_KEY = 'coach'
